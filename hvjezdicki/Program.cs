58ead2bea753602702fbe2b69ddff085d0e805ed
Oskar Józsa <oskar.jozsa@mensagymnazium.cz>
2024-10-22 17:08:51 +0200

    Add project files.

---
+++ hvjezdicki/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write(" (T_T) ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = n; i >= 1; i--)
+    {
+    for (int j = 1; j <= i; j++) 
+        {
+        Console.Write(" (T_T) ");
+        }
+    Console.WriteLine();
+    }
